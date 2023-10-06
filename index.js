9da5ea6135b06dcd7ed7b69ba76b727c6784b680
Apple <apple@matts-pro.home>
2023-10-06 13:07:54 +0100

    logic for game completed

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-function getComputersChoice() {
-    const arr = ['rock', 'paper', 'scissors']
-    let compChoice = Math.floor(Math.random() * 3)
-    const comp = arr[compChoice]
-    return comp
+ function getComputersChoice() {
+  const arr = ["rock", "paper", "scissors"];
+  let compChoice = Math.floor(Math.random() * 3);
+  const comp = arr[compChoice];
+  return comp;
 }
-console.log(getComputersChoice())
+
 
 function getPlayersChoice() {
-    const arr = ['rock', 'paper', 'scissors']
-    let playersChoice = Math.floor(Math.random() * 3)
-    const player = arr[compChoice]
-    return player
+  const arr = ["rock", "paper", "scissors"];
+  let playersChoice = Math.floor(Math.random() * 3);
+  const player = arr[playersChoice];
+  return player;
 }
-console.log(getPlayersChoice())
 
-function rockPaperScissiors(player, comp) {
-    let player1 = player.toLowerCase() 
-    if(player1 == 'rock' && comp == rock) {
-        return 'its a draw'
-    } elseif (player = 'rock' && comp == '')
 
+function rockPaperScissors(player, comp) {
+  let player1 = player.toLowerCase();
+  if (player1 == "rock" && comp == 'rock') {
+    return "its a draw";
+  } else if ((player1 == "rock" && comp == "scissors")) {
+    return "Player 1 wins";
+  } else if ((player1 == "rock" && comp == "paper")) {
+    return "Computer 1 wins";
+  } else if (player1 == "paper" && comp == "paper") {
+    return "its a draw";
+  } else if ((player1 == "paper" && comp == "scissors")) {
+    return "Computer wins";
+  } else if ((player1 == "paper" && comp == "rock")) {
+    return "Player 1 wins";
+  } else if (player1 == "scissors" && comp == "scissors") {
+    return "its a draw";
+  } else if ((player1 == "scissors" && comp == "rock")) {
+    return "Computer wins";
+  } else {
+    return "Player 1 wins";
+  }
 }
+const player = getPlayersChoice();
+const comp = getComputersChoice();
+
+console.log("Player: " + player);
+console.log("Computer: " + comp);
+console.log(rockPaperScissors(player, comp));
