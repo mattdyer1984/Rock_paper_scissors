a9a87d76797658f56c6da77fdc85834d5cc6d31c
Apple <apple@matts-pro.home>
2023-10-09 07:23:25 +0100

    UI added

--- index.js
+++ index.js
@@ -1,44 +1,72 @@
- function getComputersChoice() {
+
+function getComputersChoice() {
   const arr = ["rock", "paper", "scissors"];
   let compChoice = Math.floor(Math.random() * 3);
   const comp = arr[compChoice];
   return comp;
 }
 
+let playerScore = 0;
+let compScore = 0;
 
-function getPlayersChoice() {
-  const arr = ["rock", "paper", "scissors"];
-  let playersChoice = Math.floor(Math.random() * 3);
-  const player = arr[playersChoice];
-  return player;
-}
+function rockPaperScissors(player1, comp) {
+  let result = "";
 
-
-function rockPaperScissors(player, comp) {
-  let player1 = player.toLowerCase();
-  if (player1 == "rock" && comp == 'rock') {
-    return "its a draw";
-  } else if ((player1 == "rock" && comp == "scissors")) {
-    return "Player 1 wins";
-  } else if ((player1 == "rock" && comp == "paper")) {
-    return "Computer 1 wins";
+  if (player1 == "rock" && comp == "rock") {
+    result = "It's a draw";
+  } else if (player1 == "rock" && comp == "scissors") {
+    result = "Player 1 wins";
+    playerScore += 1;
+  } else if (player1 == "rock" && comp == "paper") {
+    result = "Computer 1 wins";
+    compScore += 1;
   } else if (player1 == "paper" && comp == "paper") {
-    return "its a draw";
-  } else if ((player1 == "paper" && comp == "scissors")) {
-    return "Computer wins";
-  } else if ((player1 == "paper" && comp == "rock")) {
-    return "Player 1 wins";
+    result = "It's a draw";
+  } else if (player1 == "paper" && comp == "scissors") {
+    result = "Computer wins";
+    compScore += 1; 
+  } else if (player1 == "paper" && comp == "rock") {
+    result = "Player 1 wins";
+    playerScore += 1; 
   } else if (player1 == "scissors" && comp == "scissors") {
-    return "its a draw";
-  } else if ((player1 == "scissors" && comp == "rock")) {
-    return "Computer wins";
+    result = "It's a draw";
+  } else if (player1 == "scissors" && comp == "rock") {
+    result = "Computer wins";
+    compScore += 1; 
   } else {
-    return "Player 1 wins";
+    result = "Player 1 wins";
+    playerScore += 1; 
   }
-}
-const player = getPlayersChoice();
-const comp = getComputersChoice();
 
-console.log("Player: " + player);
-console.log("Computer: " + comp);
-console.log(rockPaperScissors(player, comp));
+  return result;
+}
+
+function initializeGame() {
+  const playerInput = document.querySelector('#playerInput');
+  const btn = document.querySelector('#submit');
+  const resultElement = document.querySelector('#result');
+  const scoreElement = document.querySelector('#score');
+
+  btn.addEventListener('click', (e) => {
+    const player = playerInput.value;
+    const comp = getComputersChoice();
+    const result = rockPaperScissors(player, comp);
+
+    resultElement.innerHTML = "Computer chose " + comp + ", " + result;
+    scoreElement.innerHTML = "Player 1 score = " + playerScore + "<br> Computer Score = " + compScore;
+    if(playerScore == 5) {
+      scoreElement.innerHTML = "Player 1 WINS!"
+      playerScore = 0;
+      compScore = 0;
+
+    } else if(compScore == 5) {
+      scoreElement.innerHTML = "Computer 1 WINS!"
+      playerScore = 0;
+      compScore = 0;
+    }
+
+    e.preventDefault();
+  });
+}
+
+document.addEventListener('DOMContentLoaded', initializeGame);
